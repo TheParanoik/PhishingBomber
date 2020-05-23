e98770c18669efb984c799e317010e3c5e8ebe28
MPewny <41195328+MPewny@users.noreply.github.com>
2020-05-23 15:26:40 +0200

    add form

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head></head>
+  <body>
+    <form method="post" action="backend.php">
+      <input type="url" name="target">
+      <input type="text" name="login-field">
+      <input type="text" name="password-field">
+      <input type="number" name="rounds">
+      <input type="submit" value="execute">
+  </body>
+</html>
